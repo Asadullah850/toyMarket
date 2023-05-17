48f4a140eea0bdb4038f0b05f60f44fa34cfdf7f
Asadullah850 <mhshamim850@gmail.com>
2023-05-18 03:32:44 +0600

    ErrorPage done

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 
 const ErrorPage = () => {
+    const error = useRouteError()
+    console.log(error);
+
     return (
-        <div>
-            errrrrr
+        <div className="bg-[url('https://i.ibb.co/9TsK5Wm/404.gif')] h-[100vh]">
+            <div className="text-center ">
+                <div className="m-4 p-8 rounded shadow-lg">
+                    <p className="text-gray-700 mb-8 text-5xl font-bold">Oops!</p>
+                    <p className="text-gray-700 mb-8 text-2xl">{error.data}</p>
+                    <a
+                        href="/"
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+                    >
+                        Go back to Home
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
